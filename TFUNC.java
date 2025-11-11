c14ee6a9f8f49888904ab1fa9e828372b09e913e
diway127 <gstnsya@gmail.com>
2025-11-11 20:33:02 +0700

    Dewik dan Marissa

---
+++ TFUNC.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.Math;
+
+public class T00 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n, hasil;
+
+        n = Integer.parseInt(input.nextLine());
+        hasil = hitungRekursif(n);
+        System.out.println("Bilangan yang dimasukkan (N) : " + n);
+        System.out.println("Hasil perhitungan rekursif:" + hasil);
+    }
+    
+    public static int hitungRekursif(int n) {
+        int hasil;
+
+        if (n == 1) {
+            hasil = 1;
+        } else {
+            hasil = n + hitungRekursif(n - 1);
+        }
+        
+        return hasil;
+    }
+}
